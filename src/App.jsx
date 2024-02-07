833e18ede87825eaf510f20c50e8b26db8c29336
Aakamsh PM <aakamshpm@gmail.com>
2024-02-07 17:50:45 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import Footer from "./screens/Footer";
+import Header from "./screens/Header";
+import Hero from "./screens/Hero";
+
+const App = () => {
+  return (
+    <>
+      <div className="bg-black">
+        <Header />
+        <Hero />
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+export default App;
